8a90e5e20b98f33f837f8d1fb8c00f5471aa0732
Ikwu Idoko Francis <58372655+cisco00@users.noreply.github.com>
2021-05-30 08:39:11 +0100

    Update components.py
    reshaping

--- components.py
+++ components.py
@@ -1,29 +1,10 @@
+from numpy import asarray
 from PIL import Image
-import numpy as np
-from tensorflow.keras.applications.imagenet_utils import decode_predictions
-import tensorflow as tf
 
 
-def load_models():
-    models = tf.keras.applications.load_model("test_model.h5")
-    print("model loaded successfully")
-    return models
+def convert_image_to_array(file, height, width, depth):
+    with file as image:
+        image = Image.open(image)
+        image = image.resize((height, width), Image.ANTIALIAS)
+        return asarray(image).reshape((1, height, width, depth))
 
-
-def predict(image: Image.Image):
-    image = np.asarray(image.resize((224, 224)))[..., :3]
-    image = np.expand_dims(image, 0)
-    image = image / 127.5 - 1.0
-
-    result = decode_predictions(load_models.predict(image), 2)[0]
-
-    response = []
-
-    for i, res in enumerate(result):
-        resp = {}
-        resp["class"] = res[1]
-        resp["confidence"] = f"{res[2] * 100:0.2f} %"
-
-        response.append(resp)
-
-    return response
